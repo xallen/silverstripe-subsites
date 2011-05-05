67677665348bc47617b3cb1dc56f749d4abdc921
Chris Allen <chris@llen.co.nz>
2011-05-05 21:47:10 +1200

    Improved augmentSQL so that it is now not so reliant on the query itself when deciding whether augmentation is required. The new version is EXTREMELY kludgey as it involves using a backtrace to find the originating function/method, but I feel it behaves more consistently and allowed me to tune the behavior to the degree I required. The Link and Image tool in the WYSIWYG editor should now display only files and folders available to the Member and Subsite.

--- code/FileSubsites.php
+++ code/FileSubsites.php
@@ -44,58 +44,57 @@ class FileSubsites extends DataObjectDecorator {
 			if($sites)$fields->addFieldToTab('Root.Details', new DropdownField("SubsiteID", "Subsite", $dropdownValues));
 		}
 	}
-
+	
 	/**
-	 * Update any requests to limit the results to the current site
+	 * Update any requests to limit the results to the current site.
 	 */
 	function augmentSQL(SQLQuery &$query) {
-
-		// We will need to know the SubsiteID we are working with ($subsiteID)
+	
+		// We will need to know the SubsiteID we are working with ($subsiteID).
 		if($context = DataObject::context_obj()) $subsiteID = (int) $context->SubsiteID;
 		else $subsiteID = (int) Subsite::currentSubsiteID();
 	
-		/* Return if Name AND ParentID is in query. Most likely will be a call to find duplicate file names. We 
-		 *	absolutely NEED to make sure these aren't affected by SubsiteID or duplicate file names and
-		 *	folders will cause total, unholy mayhem. It was a rare occurance, but a real show stopper, and a
-		 *	total P.I.T.A to diagnose :-P I'm sorry this is such an ugly kludge! */
-		if(preg_match('/(\'|"|`|)Name(\'|"|`|).* AND .*(\'|"|`|)ParentID(\'|"|`|)/', $query->where[0])) return;
-		
-		/* Here we test for queries from TreeDropdownField and its subclasses. These queries are always(?) related
-		 *	to retrieving a list of Files and Folders for a subsite for TreeDropdownFields. E.g. LinkForm, 
-		 *	PictureStrip in WYSIWYG editor. */
-		if($query->where and $query->where[0] == '"File"."ParentID" = 0 AND "File"."ID" != 0') {
-		
-			/* Filter by SubsiteID, otherwise. If global assets are enabled, we want those too. */
-			if(Permission::check('SUBSITE_ASSETS_GLOBAL_ACCESS')) $siteList = array(-1, $subsiteID);
-			else $siteList = array($subsiteID);
-			$query->where[] = "\"SubsiteID\" IN (".implode($siteList, ',').")";
-		}
-		
-		// If you're querying by ID, ignore the sub-site - this is a bit ugly... (but it was WAYYYYYYYYY worse)
-		// (If no WHERE clauses, or no mention of ."ID")
-		if(!$query->where || !preg_match('/\.(\'|"|`|)ID(\'|"|`|)/', $query->where[0])) {
-
-			// The foreach is an ugly way of getting the first key :-)
-			foreach($query->from as $tableName => $info) {
-				if(Permission::check('SUBSITE_ASSETS_GLOBAL_ACCESS')) {
-					$siteList = array(-1, $subsiteID);
-				} else {
-					$siteList = array($subsiteID);
-				}
-				
-				$where = "\"$tableName\".\"SubsiteID\" IN (".implode($siteList, ',').")";
-				$query->where[] = $where;
+		// A list of functions to filter out of the backtrace.
+		$function_filters = array('augmentSQL', 'extend', 'extendedSQL', 'getQuery', 'get', 'instance_get');
+	
+		/* Find the last called function (that doesn't match a filter) and store it in $calling_function. We do
+		 * 	this using debug_backtrace(). It should be a lot more accurate to match queries for augmentation this
+		 *	way rather than by the query itself. */
+		$backtrace = debug_backtrace();
+		foreach($backtrace as $key => $trace) {
+			if(!in_array($trace['function'], $function_filters)) {
+				$calling_function = $trace['function'];
 				break;
 			}
-			
-			$isCounting = strpos($query->select[0], 'COUNT') !== false;
-
-			// Ordering when deleting or counting doesn't apply
-			if(!$query->delete && !$isCounting) {
-				$query->orderby = "\"SubsiteID\"" . ($query->orderby ? ', ' : '') . $query->orderby;
-			}			
 		}
 		
+		// Take appropriate action depending on the method operating on this DataObject.
+		switch($calling_function) {
+		
+			case 'ChildFolders': // Used to filter the File and Folder list in AssetAdmin.
+			case 'stageChildren': // Used to filter the File and Folder list for the Image and Link WYSIWYG tools.
+				// The foreach is an ugly way of getting the first key :-)
+				foreach($query->from as $tableName => $info) {
+					if(Permission::check('SUBSITE_ASSETS_GLOBAL_ACCESS')) {
+						$siteList = array(-1, $subsiteID);
+					} else {
+						$siteList = array($subsiteID);
+					}
+					
+					$where = "\"$tableName\".\"SubsiteID\" IN (".implode($siteList, ',').")";
+					$query->where[] = $where;
+					break;
+				}
+				return;
+			case 'numChildren': // COUNT?.
+			case 'instance_get_one': // instance_get_one seems to be invoked only when retrieving a single DataObject.
+				// NOTE: This is used when testing if a file or folder already exists. Subsites module now correctly avoids duplicate folder names!
+			case 'isFieldSortable': // isFieldSortable is a method in TableFieldList.
+			case 'sourceItems': // Major player in TableListField.
+			case 'TotalCount': // TotalCount is used to count the number of available files.
+			default:
+				return;
+		}
 	}
 
 	function onBeforeWrite() {
@@ -116,12 +115,14 @@ class FileSubsites extends DataObjectDecorator {
 		if($this->owner->Parent()->ParentID == '' and !Permission::check('SUBSITE_ASSETS_ROOT_WRITE')) {
 			//unlink($this->owner->Parent()->Filename); // Delete the file as we don't need it any longer
 			//exit('errorMessage("You do not have permission to create, modify or delete a folder or file in this location.");');
+			//exit('statusMessage("You do not have permission to create, modify or delete a folder or file in this location.");');
 		}
 		
 		// Abort write if member cannot write to global assets and destination is global assets
 		if($this->owner->Parent()->SubsiteID == -1 and !Permission::check('SUBSITE_ASSETS_GLOBAL_WRITE')) {
 			//unlink($this->owner->Parent()->Filename); // Delete the file as we don't need it any longer
 			//exit('errorMessage("You do not have permission to create, modify or delete a folder or file in this location.");');
+			//exit('statusMessage("You do not have permission to create, modify or delete a folder or file in this location.");'); 
 		}
 	
 		// If we have a parent, use it's subsite as our subsite
